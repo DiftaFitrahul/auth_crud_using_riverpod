1a71b9088584279fe132457e4f1b9859edd61bcd
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-02-14 18:16:51 +0700

    feat: finish create logincontroller

---
+++ lib/vm/login_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learn_firebase_riverpod/providers/auth_provider.dart';
+import 'package:learn_firebase_riverpod/vm/login_state.dart';
+
+class LoginController extends StateNotifier<LoginState> {
+  LoginController(this.ref) : super(const LoginStateInitial());
+
+  final Ref ref;
+
+  void signIn(String email, String password) async {
+    state = const LoginStateLoading();
+    try {
+      await ref
+          .read(authServiceProvider)
+          .signInWithEmailandPassword(email, password);
+      state = const LoginStateSuccess();
+    } catch (e) {
+      state = LoginStateError(e.toString());
+    }
+  }
+}
+
+final loginControllerProvider =
+    StateNotifierProvider<LoginController, LoginState>(
+        (ref) => LoginController(ref));
